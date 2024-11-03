68b63647f999e2990b3926ba7542d821c303e3f7
James Unthank <james@unfrank.com>
2024-11-03 17:03:04 -0500

    ItemModal Footer ModalWithForm

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -65,7 +65,7 @@ function App() {
               type="text"
               className="modal__input"
               id="name"
-              placeholder="name"
+              placeholder="Name"
             />
           </label>
           <label htmlFor="imageUrl" className="modal__label">
@@ -74,35 +74,25 @@ function App() {
               type="url"
               className="modal__input"
               id="imageUrl"
-              placeholder="Image
-            URL"
+              placeholder="Image URL"
             />
           </label>
           <fieldset className="modal__fieldset">
-            <legend className="modal__legend">Select the Weather Type</legend>
+            <legend className="modal__legend">Select the Weather Type:</legend>
 
-            <label
-              htmlFor="hot"
-              className="modal__label modal__label_type_radio"
-            >
+            <label htmlFor="hot" className="modal__label_type_radio">
               <input id="hot" type="radio" className="modal__radio-input" />
-              Hot
+              <span className="modal__label_text">Hot</span>
             </label>
 
-            <label
-              htmlFor="warm"
-              className="modal__label modal__label_type_radio"
-            >
+            <label htmlFor="warm" className="modal__label_type_radio">
               <input id="warm" type="radio" className="modal__radio-input" />
-              Warm
+              <span className="modal__label_text">Warm</span>
             </label>
 
-            <label
-              htmlFor="cold"
-              className="modal__label modal__label_type_radio"
-            >
+            <label htmlFor="cold" className="modal__label_type_radio">
               <input id="cold" type="radio" className="modal__radio-input" />
-              Cold
+              <span className="modal__label_text">Cold</span>
             </label>
           </fieldset>
         </ModalWithForm>
@@ -117,3 +107,5 @@ function App() {
 }
 
 export default App;
+
+// ! // ! REMEMBER! Removed modal__label class from each of the 3 radios. orig: className="modal__label modal__label_type_radio"
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,4 +1,5 @@
 import "./ModalWithForm.css";
+import closeBtn from "../../assets/close-btn--dark.png";
 
 function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
   return (
@@ -8,7 +9,7 @@ function ModalWithForm({ children, buttonText, title, activeModal, onClose }) {
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button onClick={onClose} type="button" className="modal__close">
-          close
+          <img src={closeBtn} alt="Close" />
         </button>
         <form className="modal__form">
           {children}
